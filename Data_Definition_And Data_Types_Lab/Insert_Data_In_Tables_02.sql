c1b0716c0769aaf8edb597bfa227927786bfb1d1
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-01-26 20:55:56 +0200

    Add files via upload

---
+++ Data_Definition_And Data_Types_Lab/Insert_Data_In_Tables_02.sql
@@ -0,0 +1,3 @@
+INSERT INTO `employees`(`id`, `first_name`, `last_name`) VALUES (1, 'Denis', 'Petrov');
+INSERT INTO `employees`(`id`, `first_name`, `last_name`) VALUES (2, 'Plamen', 'Ivanov');
+INSERT INTO `employees`(`id`, `first_name`, `last_name`) VALUES (3, 'Ivana', 'Todorova');
