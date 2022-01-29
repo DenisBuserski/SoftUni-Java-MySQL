2766e6298f3ba2f0835562c1198a4b52c3068086
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-01-29 20:02:32 +0200

    Add files via upload

---
+++ Basic_CRUD_Exercise/Countries_And_Currency_Euro_Not_Euro_23.sql
@@ -0,0 +1,9 @@
+SELECT 
+    `country_name`,
+    `country_code`,
+    IF(`currency_code` != 'EUR',
+        'Not Euro',
+        'Euro') AS `currency`
+FROM
+    `countries`
+ORDER BY `country_name` ASC;
