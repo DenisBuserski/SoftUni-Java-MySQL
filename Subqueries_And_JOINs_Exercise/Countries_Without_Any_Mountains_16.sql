f4dc6ba398f2dfa80a7d64b90b80d78744c902e2
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-01 18:46:09 +0200

    Add files via upload

---
+++ Subqueries_And_JOINs_Exercise/Countries_Without_Any_Mountains_16.sql
@@ -0,0 +1,7 @@
+SELECT 
+    (COUNT(`c`.`country_name`) - COUNT(`mc`.`country_code`)) AS `country_count`
+FROM
+    `countries` AS `c`
+        LEFT JOIN
+    `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`;
+
