3fc98ba96bc17cc6b7f29b2f9a16f193f4a0af58
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-06 10:40:38 +0200

    Add files via upload

---
+++ Database_Programmability_Exercise/Town_Names_Starting_With_03.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE usp_get_towns_starting_with(str VARCHAR(5))
+BEGIN
+SELECT `name` AS `town_name`
+FROM `towns` AS `t`
+WHERE `t`.`name` LIKE CONCAT(str, '%')
+ORDER BY `town_name` ASC;
+END
