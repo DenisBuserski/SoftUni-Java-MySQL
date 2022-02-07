621956ad90ff2e5cd295170ced285d65f5d8835d
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-07 08:51:21 +0200

    Add files via upload

---
+++ Database_Programmability_Exercise/People_With_Balance_Higher_Than_09.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(salary INT)
+BEGIN
+SELECT `first_name`, `last_name`
+FROM `account_holders` AS `ah` 
+JOIN `accounts` AS `a` ON `ah`.`id` = `a`.`account_holder_id`
+GROUP BY `ah`.`id`
+HAVING SUM(`balance`) > salary
+ORDER BY `ah`.`id`;
+END
