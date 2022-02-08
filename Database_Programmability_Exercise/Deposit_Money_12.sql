e3182e3ebc31efee5042bf4e5dff9c2081082f75
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-08 16:27:28 +0200

    Add files via upload

---
+++ Database_Programmability_Exercise/Deposit_Money_12.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DECIMAL(20,4))
+BEGIN
+START TRANSACTION;
+	CASE WHEN money_amount < 0 
+		THEN ROLLBACK;
+	ELSE 
+		UPDATE accounts as a 
+		SET a.balance = a.balance + money_amount
+        	WHERE a.id = account_id;
+	END CASE;
+COMMIT;
+END
