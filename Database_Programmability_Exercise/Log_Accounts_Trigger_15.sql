9da157400f8013da4e433b6b237db2f02e3f9116
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-08 16:29:11 +0200

    Add files via upload

---
+++ Database_Programmability_Exercise/Log_Accounts_Trigger_15.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `logs` (
+    `log_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `account_id` INT NOT NULL,
+    `old_sum` DECIMAL(19 , 4 ) NOT NULL,
+    `new_sum` DECIMAL(19 , 4 ) NOT NULL
+);
+
+
+CREATE TRIGGER trigger_balance_update
+AFTER UPDATE ON `accounts`
+FOR EACH ROW
+BEGIN
+IF OLD.balance <> NEW.balance 
+THEN 
+INSERT INTO `logs`(account_id, old_sum, new_sum) 
+VALUES (OLD.id, OLD.balance, NEW.balance);
+END IF;
+END;
