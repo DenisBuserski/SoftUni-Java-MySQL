19d25b6da4614f8a06c61a30b59ce2b5f5beec2a
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-08 16:27:59 +0200

    Add files via upload

---
+++ Database_Programmability_Exercise/Withdraw_Money_13.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(20,4))
+BEGIN
+START TRANSACTION;
+	CASE WHEN money_amount < 0 OR 
+              money_amount > (SELECT a.balance FROM accounts as a
+						      WHERE a.id = account_id)
+		THEN ROLLBACK;
+	ELSE 
+		UPDATE accounts as a 
+		SET a.balance = a.balance - money_amount
+        	WHERE a.id = account_id;
+	END CASE;
+COMMIT;
+END
