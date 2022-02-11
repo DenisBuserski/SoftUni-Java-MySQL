ce52fc33ffbe7704a54bb3960258ae78f8445fc0
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-11 12:07:46 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Retake_Exam_31_March_2020/Lucky_Users_07.sql
@@ -0,0 +1,10 @@
+SELECT 
+    CONCAT_WS(' ', `u`.`id`, `u`.`username`) AS `id_username`,
+    `email`
+FROM
+    `users` AS `u`
+        JOIN
+    `users_photos` AS `up` ON `u`.`id` = `up`.`user_id`
+WHERE
+    `up`.`photo_id` = `u`.`id`
+ORDER BY `u`.`id` ASC;
