7ecf1a94dc3946409293766cbd70ab903d33b543
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-13 08:41:24 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_20_June_2021/Number_Of_Courses_07.sql
@@ -0,0 +1,13 @@
+SELECT
+`c`.`id` AS `car_id`,
+`c`.`make`,
+`c`.`mileage`,
+COUNT(`cour`.`from_address_id`) AS `count_of_courses`,
+ROUND(AVG(`cour`.`bill`), 2) AS `avg_bill`
+FROM `cars` AS `c`
+LEFT JOIN `courses` AS `cour` ON `c`.`id` = `cour`.`car_id`
+GROUP BY `c`.`id`
+HAVING `count_of_courses` != 2
+ORDER BY 
+`count_of_courses` DESC,
+`car_id` ASC;
