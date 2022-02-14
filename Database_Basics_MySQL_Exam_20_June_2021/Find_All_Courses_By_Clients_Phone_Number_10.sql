3bf862983e9915b000259b8b96c6e546fdc5f098
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-14 11:15:55 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_20_June_2021/Find_All_Courses_By_Clients_Phone_Number_10.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION udf_courses_by_client (phone_num VARCHAR (20)) 
+RETURNS VARCHAR(50)
+DETERMINISTIC 
+BEGIN
+RETURN 
+(SELECT
+COUNT(`from_address_id`)
+FROM `courses` AS `co` LEFT JOIN `clients` AS `cl` ON `co`.`client_id` = `cl`.`id`
+WHERE `cl`.`phone_number` = phone_num);
+END
