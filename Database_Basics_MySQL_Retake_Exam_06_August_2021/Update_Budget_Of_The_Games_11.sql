0c2dff789b9841202aeb23627672ed2b963535c8
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-16 08:07:15 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Retake_Exam_06_August_2021/Update_Budget_Of_The_Games_11.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE udp_update_budget(min_game_rating FLOAT) 
+BEGIN
+UPDATE `games`
+SET 
+`budget` = `budget` + 100000,
+`release_date` = DATE_ADD(`release_date`, INTERVAL 1 YEAR)
+WHERE
+`release_date` IS NOT NULL AND 
+`id` NOT IN (SELECT `game_id` FROM `games_categories`) AND
+`rating` > min_game_rating;
+END
