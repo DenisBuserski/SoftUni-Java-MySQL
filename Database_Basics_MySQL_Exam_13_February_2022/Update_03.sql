a625c8d805fafb3beb18d950b5dcda0cdce52564
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-16 08:11:24 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_13_February_2022/Update_03.sql
@@ -0,0 +1,10 @@
+UPDATE `products` 
+SET 
+    `quantity_in_stock` = `quantity_in_stock` - 5
+WHERE
+    (`quantity_in_stock` >= 60
+        AND `quantity_in_stock` <= 70
+        AND `id` <> 0);
+
+
+
