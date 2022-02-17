ace17c919866ceb7680f0ea4b131df183a7cdd29
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-17 08:14:23 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_13_February_2022/Extract_client_cards_count_10.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION udf_customer_products_count(name VARCHAR(30)) 
+RETURNS VARCHAR(50)
+DETERMINISTIC 
+BEGIN
+RETURN 
+(SELECT
+COUNT(`op`.`product_id`) AS `total_products`
+FROM `customers` AS `c` JOIN `orders` AS `o` ON `c`.`id` = `o`.`customer_id` 
+JOIN `orders_products` AS `op` ON `op`.`order_id` = `o`.`id`
+WHERE `c`.`first_name` = name);
+END
+
