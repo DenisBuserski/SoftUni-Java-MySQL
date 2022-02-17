fdc73671033ff470b25227277e08d88c51926bbd
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-17 08:13:43 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_13_February_2022/Best_Categories_09.sql
@@ -0,0 +1,11 @@
+SELECT 
+    COUNT(`p`.`id`) AS `items_count`,
+    `c`.`name`,
+    SUM(`p`.`quantity_in_stock`) AS `total_quantity`
+FROM
+    `products` AS `p`
+        LEFT JOIN
+    `categories` AS `c` ON `p`.`category_id` = `c`.`id`
+GROUP BY `c`.`name`
+ORDER BY `items_count` DESC , `total_quantity` ASC
+LIMIT 5;
