6986027caec51104f9ca2a622fd49623970ab6fe
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-24 15:25:38 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_9_Jun_2019/Extract_Cards_Count_10.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION udf_client_cards_count(name VARCHAR(30)) 
+RETURNS INT
+DETERMINISTIC 
+BEGIN
+RETURN 
+(SELECT
+COUNT(`cards`.`id`)
+FROM `clients` AS `c`
+RIGHT JOIN `bank_accounts` AS `ba` ON `c`.`id` = `ba`.`client_id`
+RIGHT JOIN `cards` ON `ba`.`id` = `cards`.`bank_account_id`
+WHERE `c`.`full_name` = name);
+END
