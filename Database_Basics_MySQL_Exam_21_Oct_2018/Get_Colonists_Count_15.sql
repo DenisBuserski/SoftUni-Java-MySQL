29f47b6e1148b9007a6b7010e3e5030206ebaa4b
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-26 07:49:50 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_21_Oct_2018/Get_Colonists_Count_15.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION udf_count_colonists_by_destination_planet (planet_name VARCHAR (30))
+RETURNS INT
+DETERMINISTIC 
+BEGIN
+RETURN 
+(SELECT
+COUNT(`c`.`id`)
+FROM `planets` AS `p`
+JOIN `spaceports` AS `sp` ON `p`.`id` = `sp`.`planet_id`
+JOIN `journeys` AS `j` ON `sp`.`id` = `j`.`destination_spaceport_id`
+JOIN `travel_cards` AS `tc` ON `j`.`id` = `tc`.`journey_id`
+JOIN `colonists` AS `c` ON `tc`.`colonist_id` = `c`.`id` 
+WHERE `p`.`name` = planet_name);
+END
+
+
