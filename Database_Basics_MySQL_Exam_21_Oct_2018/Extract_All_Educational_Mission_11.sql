5dca393dc7faac1077662dc2d612a7268476f7f1
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-02-26 07:46:30 +0200

    Add files via upload

---
+++ Database_Basics_MySQL_Exam_21_Oct_2018/Extract_All_Educational_Mission_11.sql
@@ -0,0 +1,11 @@
+SELECT 
+    `p`.`name` AS `planet_name`, `sp`.`name` AS `spaceport_name`
+FROM
+    `planets` AS `p`
+        JOIN
+    `spaceports` AS `sp` ON `p`.`id` = `sp`.`planet_id`
+        JOIN
+    `journeys` AS `j` ON `j`.`destination_spaceport_id` = `sp`.`id`
+WHERE
+    `j`.`purpose` = 'Educational'
+ORDER BY `spaceport_name` DESC;
