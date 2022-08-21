4ababa07ea73e79f63281d1711b5224d968b0e15
Denis Buserski <denis.buserski@gmail.com>
2022-08-21 19:36:42 +0300

    Update 09_Profile_Pictures.sql

--- Instagraph_mysql_exam/09_Profile_Pictures.sql
+++ Instagraph_mysql_exam/09_Profile_Pictures.sql
@@ -1,3 +1,10 @@
 -- 09.Profile Pictures
 
 
+SELECT `u`.`id`, 
+`u`.`username`, 
+CONCAT_WS(' ', `p`.`size`, 'KB') AS `size`
+FROM `users` AS `u` JOIN `pictures` AS `p` ON `u`.`profile_picture_id` = `p`.`id`
+WHERE `profile_picture_id` > 1
+GROUP BY `profile_picture_id`
+ORDER BY `p`.`id` ASC;
