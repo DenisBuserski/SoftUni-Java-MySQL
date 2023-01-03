bfc3d395e73b5195a6e11f0d1404e6048ae1655e
Denis Buserski <denis.buserski@gmail.com>
2023-01-03 23:18:11 +0200

    Update and rename Data_Definition_And_Data_Types_Exercise/Movies_Database_11.sql to Data_Definition_And_Data_Types/Exercise/Movies_Database_11.sql

--- Data_Definition_And_Data_Types/Exercise/Movies_Database_11.sql
+++ Data_Definition_And_Data_Types/Exercise/Movies_Database_11.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `directors` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `director_name` VARCHAR(50) NOT NULL,
-    `notes` TEXT
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`director_name` VARCHAR(50) NOT NULL,
+`notes` TEXT
 ); 
 
 INSERT INTO `directors`(`director_name`, `notes`)
@@ -13,9 +13,9 @@ VALUES
 ('TestName5', 'TestNotes');
 
 CREATE TABLE `genres` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `genre_name` VARCHAR(20) NOT NULL,
-    `notes` TEXT
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`genre_name` VARCHAR(20) NOT NULL,
+`notes` TEXT
 );
 
 INSERT INTO `genres`(`genre_name`, `notes`)
@@ -27,9 +27,9 @@ VALUES
 ('TestName5', 'TestNotes');
 
 CREATE TABLE `categories` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `category_name` VARCHAR(20) NOT NULL,
-    `notes` TEXT
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`category_name` VARCHAR(20) NOT NULL,
+`notes` TEXT
 );
 
 INSERT INTO `categories`(`category_name`, `notes`)
@@ -41,15 +41,15 @@ VALUES
 ('TestName5', 'TestNotes');
 
 CREATE TABLE `movies` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `title` VARCHAR(40) NOT NULL,
-    `director_id` INT,
-    `copyright_year` INT,
-    `length` INT,
-    `genre_id` INT,
-    `category_id` INT,
-    `rating` DOUBLE,
-    `notes` TEXT
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`title` VARCHAR(40) NOT NULL,
+`director_id` INT,
+`copyright_year` INT,
+`length` INT,
+`genre_id` INT,
+`category_id` INT,
+`rating` DOUBLE,
+`notes` TEXT
 );
 
 INSERT INTO `movies` (`title`)
