e74bcedaa4b98343d94f6fd20bd2bd5a4cefd547
Denis Buserski <denis.buserski@gmail.com>
2023-01-10 20:01:09 +0200

    Update and rename Basic_CRUD_Exercise/Create_View_Employees_With_Job_Titles_16.sql to Basic_CRUD/Exercise/Create_View_Employees_With_Job_Titles_16.sql

--- Basic_CRUD_Exercise/Create_View_Employees_With_Job_Titles_16.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW v_employees_job_titles AS
-    SELECT 
-        CONCAT_WS(' ',
-                `first_name`,
-                `middle_name`,
-                `last_name`) AS 'full_name',
-        `job_title`
-    FROM
-        `employees`;
