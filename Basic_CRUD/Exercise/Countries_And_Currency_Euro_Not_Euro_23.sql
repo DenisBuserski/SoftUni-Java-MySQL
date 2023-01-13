d5e87c990a2aa47b680bfac6aecf9e63e5dd7870
Denis Buserski <denis.buserski@gmail.com>
2023-01-13 22:12:45 +0200

    Update and rename Basic_CRUD_Exercise/Countries_And_Currency_Euro_Not_Euro_23.sql to Basic_CRUD/Exercise/Countries_And_Currency_Euro_Not_Euro_23.sql

---
+++ Basic_CRUD/Exercise/Countries_And_Currency_Euro_Not_Euro_23.sql
@@ -0,0 +1,6 @@
+SELECT 
+`country_name`,
+`country_code`,
+IF(`currency_code` != 'EUR', 'Not Euro', 'Euro') AS `currency`
+FROM `countries`
+ORDER BY `country_name` ASC;
--- Basic_CRUD_Exercise/Countries_And_Currency_Euro_Not_Euro_23.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    `country_name`,
-    `country_code`,
-    IF(`currency_code` != 'EUR',
-        'Not Euro',
-        'Euro') AS `currency`
-FROM
-    `countries`
-ORDER BY `country_name` ASC;
