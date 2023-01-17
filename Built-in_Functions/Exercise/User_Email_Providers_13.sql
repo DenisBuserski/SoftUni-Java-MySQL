5c2a659df31074a0ea6bb2b2f37f7ac44279ca3d
Denis Buserski <denis.buserski@gmail.com>
2023-01-17 23:01:22 +0200

    Update and rename Built-in_Functions_Exercise/User_Email_Providers_13.sql to Built-in_Functions/Exercise/User_Email_Providers_13.sql

---
+++ Built-in_Functions/Exercise/User_Email_Providers_13.sql
@@ -0,0 +1,7 @@
+SELECT 
+`user_name`,
+REGEXP_REPLACE(`email`, '.*@', '') AS `email provider`
+FROM `users`
+ORDER BY 
+`email provider`, 
+`user_name`;
--- Built-in_Functions_Exercise/User_Email_Providers_13.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    `user_name`,
-    REGEXP_REPLACE(`email`, '.*@', '') AS `email provider`
-FROM
-    `users`
-ORDER BY `email provider` , `user_name`;
