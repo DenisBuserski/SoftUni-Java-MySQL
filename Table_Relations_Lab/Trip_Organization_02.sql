cb22bfb5df8ae0d97abe75201cbc581d53f7a0da
Denis Buserski <denis.buserski@gmail.com>
2023-01-20 23:08:26 +0200

    Update and rename Table_Relations_Lab/Trip_Organization_02.sql to Table_Relations/Lab/Trip_Organization_02.sql

--- Table_Relations_Lab/Trip_Organization_02.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    `v`.`driver_id`,
-    `v`.`vehicle_type`,
-    CONCAT(`c`.`first_name`, ' ', `c`.`last_name`) AS `driver_name`
-FROM
-    `vehicles` AS `v`,
-    `campers` AS `c`
-WHERE
-    `c`.`id` = `v`.`driver_id`;
