51f559b33eee32756febda1152fd130c08619c87
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:59:47 +0200

    Update Countries_Without_Any_Mountains_16.sql

--- Subqueries_And_JOINs/Exercise/Countries_Without_Any_Mountains_16.sql
+++ Subqueries_And_JOINs/Exercise/Countries_Without_Any_Mountains_16.sql
@@ -1,7 +1,4 @@
 SELECT 
-    (COUNT(`c`.`country_name`) - COUNT(`mc`.`country_code`)) AS `country_count`
-FROM
-    `countries` AS `c`
-        LEFT JOIN
-    `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`;
-
+(COUNT(`c`.`country_name`) - COUNT(`mc`.`country_code`)) AS `country_count`
+FROM `countries` AS `c`
+LEFT JOIN `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`;
