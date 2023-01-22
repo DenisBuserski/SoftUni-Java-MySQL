6b5d2b8470031158cba62b69d85ab669db3907a6
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:56:11 +0200

    Update Min_Average_Salary_11.sql

--- Subqueries_And_JOINs/Exercise/Min_Average_Salary_11.sql
+++ Subqueries_And_JOINs/Exercise/Min_Average_Salary_11.sql
@@ -1,9 +1,3 @@
-SELECT 
-    MIN(`min-avg`) AS `min_average_salary`
+SELECT MIN(`min-avg`) AS `min_average_salary`
 FROM
-    (SELECT 
-        AVG(`salary`) AS `min-avg`
-    FROM
-        `employees`
-    GROUP BY `department_id`) AS `a`;
-
+(SELECT AVG(`salary`) AS `min-avg` FROM `employees` GROUP BY `department_id`) AS `a`;
