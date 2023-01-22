ca9a7b416420cbc297a7a10a945833f783b7583d
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:53:47 +0200

    Update Employee_24_08.sql

--- Subqueries_And_JOINs/Exercise/Employee_24_08.sql
+++ Subqueries_And_JOINs/Exercise/Employee_24_08.sql
@@ -2,10 +2,9 @@ SELECT
 `e`.`employee_id`,
 `e`.`first_name`,
 (IF (`p`.`start_date` >= '2005-01-01', NULL, `p`.`name`)) AS `project_name`
-FROM `employees` AS `e` 
+FROM 
+`employees` AS `e` 
 JOIN `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
 JOIN `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
-WHERE 
-`e`.`employee_id` = 24
-ORDER BY 
-`p`.`name` ASC;
+WHERE `e`.`employee_id` = 24
+ORDER BY `p`.`name` ASC;
