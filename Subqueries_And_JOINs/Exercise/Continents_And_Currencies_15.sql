bb39a58ef23a35c1fc898111158593dabcd41e53
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:59:07 +0200

    Update Continents_And_Currencies_15.sql

--- Subqueries_And_JOINs/Exercise/Continents_And_Currencies_15.sql
+++ Subqueries_And_JOINs/Exercise/Continents_And_Currencies_15.sql
@@ -1,17 +1,15 @@
 SELECT 
-    `c`.`continent_code`,
-    `c`.`currency_code`,
-    COUNT(*) AS `currency_usage`
-FROM
-    `countries` AS `c`
+`c`.`continent_code`,
+`c`.`currency_code`,
+COUNT(*) AS `currency_usage`
+FROM `countries` AS `c`
 GROUP BY `c`.`continent_code` , `c`.`currency_code`
-HAVING `currency_usage` > 1
-    AND `currency_usage` = (SELECT 
-        COUNT(*) AS `most_used_curr`
-    FROM
-        `countries` AS `c2`
-    WHERE
-        `c2`.`continent_code` = `c`.`continent_code`
+HAVING 
+`currency_usage` > 1
+AND `currency_usage` = 
+   (SELECT COUNT(*) AS `most_used_curr`
+    FROM `countries` AS `c2`
+    WHERE `c2`.`continent_code` = `c`.`continent_code`
     GROUP BY `c2`.`currency_code`
     ORDER BY `most_used_curr` DESC
     LIMIT 1)
