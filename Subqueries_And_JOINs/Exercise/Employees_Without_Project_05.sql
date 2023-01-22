a72b6464d3e7d4742e2d5cf28e999cd2705143d4
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:51:18 +0200

    Update Employees_Without_Project_05.sql

--- Subqueries_And_JOINs/Exercise/Employees_Without_Project_05.sql
+++ Subqueries_And_JOINs/Exercise/Employees_Without_Project_05.sql
@@ -1,13 +1,8 @@
 SELECT 
-    `employee_id`, `first_name`
-FROM
-    `employees` AS `e`
+`employee_id`, 
+`first_name`
+FROM `employees` AS `e`
 WHERE
-    NOT EXISTS( SELECT 
-            `employee_id`
-        FROM
-            `employees_projects` AS `ep`
-        WHERE
-            `e`.`employee_id` = `ep`.`employee_id`)
+NOT EXISTS( SELECT `employee_id` FROM `employees_projects` AS `ep` WHERE `e`.`employee_id` = `ep`.`employee_id`)
 ORDER BY `e`.`employee_id` DESC
 LIMIT 3;
