3b182734fde0a70df52f251628e2f0ba0c294d31
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:49:18 +0200

    Update Sales_Employee_03.sql

--- Subqueries_And_JOINs/Exercise/Sales_Employee_03.sql
+++ Subqueries_And_JOINs/Exercise/Sales_Employee_03.sql
@@ -1,12 +1,9 @@
 SELECT 
-    `e`.`employee_id`,
-    `e`.`first_name`,
-    `e`.`last_name`,
-    `d`.`name` AS `department_name`
-FROM
-    `employees` AS `e`
-        JOIN
-    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
-WHERE
-    `d`.`name` LIKE 'Sales'
+`e`.`employee_id`,
+`e`.`first_name`,
+`e`.`last_name`,
+`d`.`name` AS `department_name`
+FROM `employees` AS `e`
+JOIN `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE `d`.`name` LIKE 'Sales'
 ORDER BY `employee_id` DESC;
