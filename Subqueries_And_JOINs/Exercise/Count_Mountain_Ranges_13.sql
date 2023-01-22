05e8728cc219ae78059165a57c789efce143b259
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:57:21 +0200

    Update Count_Mountain_Ranges_13.sql

--- Subqueries_And_JOINs/Exercise/Count_Mountain_Ranges_13.sql
+++ Subqueries_And_JOINs/Exercise/Count_Mountain_Ranges_13.sql
@@ -1,13 +1,11 @@
 SELECT 
-    `mc`.`country_code`,
-    COUNT(`m`.`mountain_range`) AS `mountain_range`
-FROM
-    `mountains_countries` AS `mc`
-        JOIN
-    `mountains` AS `m` ON `mc`.`mountain_id` = `m`.`id`
+`mc`.`country_code`,
+COUNT(`m`.`mountain_range`) AS `mountain_range`
+FROM `mountains_countries` AS `mc`
+JOIN `mountains` AS `m` ON `mc`.`mountain_id` = `m`.`id`
 WHERE
-    `mc`.`country_code` = 'BG'
-        OR `mc`.`country_code` = 'US'
-        OR `mc`.`country_code` = 'RU'
+`mc`.`country_code` = 'BG'
+OR `mc`.`country_code` = 'US'
+OR `mc`.`country_code` = 'RU'
 GROUP BY `mc`.`country_code`
 ORDER BY `mountain_range` DESC;
