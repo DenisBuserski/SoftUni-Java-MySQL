5b503fd2a3d293e13e9f84bbc09725ff4544e49c
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 17:52:57 +0200

    Update Employees_With_Project_07.sql

--- Subqueries_And_JOINs/Exercise/Employees_With_Project_07.sql
+++ Subqueries_And_JOINs/Exercise/Employees_With_Project_07.sql
@@ -1,15 +1,15 @@
 SELECT 
-    `e`.`employee_id`,
-    `e`.`first_name`,
-    `p`.`name` AS `project_name`
+`e`.`employee_id`,
+`e`.`first_name`,
+`p`.`name` AS `project_name`
 FROM
-    `employees` AS `e`
-        JOIN
-    `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
-        JOIN
-    `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
+`employees` AS `e`
+JOIN `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+JOIN `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
 WHERE
-    DATE(`p`.`start_date`) > '2002-08-13'
-        AND `p`.`end_date` IS NULL
-ORDER BY `first_name` ASC , `project_name` ASC
+DATE(`p`.`start_date`) > '2002-08-13'
+AND `p`.`end_date` IS NULL
+ORDER BY 
+`first_name` ASC, 
+`project_name` ASC
 LIMIT 5;
