f0443df179881af47474a235ed0659daf22cfffb
Denis Buserski <denis.buserski@gmail.com>
2023-01-22 18:00:45 +0200

    Update Highest_Peak_And_Longest_River_By_Country_17.sql

--- Subqueries_And_JOINs/Exercise/Highest_Peak_And_Longest_River_By_Country_17.sql
+++ Subqueries_And_JOINs/Exercise/Highest_Peak_And_Longest_River_By_Country_17.sql
@@ -1,17 +1,16 @@
 SELECT 
-    `c`.`country_name`,
-    MAX(`p`.`elevation`) AS `highest_peak_elevation`,
-    MAX(`r`.`length`) AS `longest_river_length`
+`c`.`country_name`,
+MAX(`p`.`elevation`) AS `highest_peak_elevation`,
+MAX(`r`.`length`) AS `longest_river_length`
 FROM
-    `countries` AS `c`
-        LEFT JOIN
-    `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
-        LEFT JOIN
-    `peaks` AS `p` ON `p`.`mountain_id` = `mc`.`mountain_id`
-        LEFT JOIN
-    `countries_rivers` AS `cr` ON `c`.`country_code` = `cr`.`country_code`
-        LEFT JOIN
-    `rivers` AS `r` ON `r`.`id` = `cr`.`river_id`
+`countries` AS `c`
+LEFT JOIN `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
+LEFT JOIN `peaks` AS `p` ON `p`.`mountain_id` = `mc`.`mountain_id`
+LEFT JOIN `countries_rivers` AS `cr` ON `c`.`country_code` = `cr`.`country_code`
+LEFT JOIN `rivers` AS `r` ON `r`.`id` = `cr`.`river_id`
 GROUP BY `c`.`country_code`
-ORDER BY `highest_peak_elevation` DESC , `longest_river_length` DESC , `country_name` ASC
+ORDER BY 
+`highest_peak_elevation` DESC, 
+`longest_river_length` DESC, 
+`country_name` ASC
 LIMIT 5;
