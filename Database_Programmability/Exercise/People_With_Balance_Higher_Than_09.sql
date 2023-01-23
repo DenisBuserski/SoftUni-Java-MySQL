62fcdfe67fbd3be201ef81bfbc4811ba42ab3a04
Denis Buserski <denis.buserski@gmail.com>
2023-01-23 21:32:30 +0200

    Update People_With_Balance_Higher_Than_09.sql

--- Database_Programmability/Exercise/People_With_Balance_Higher_Than_09.sql
+++ Database_Programmability/Exercise/People_With_Balance_Higher_Than_09.sql
@@ -1,6 +1,8 @@
 CREATE PROCEDURE usp_get_holders_with_balance_higher_than(salary INT)
 BEGIN
-SELECT `first_name`, `last_name`
+SELECT 
+`first_name`, 
+`last_name`
 FROM `account_holders` AS `ah` 
 JOIN `accounts` AS `a` ON `ah`.`id` = `a`.`account_holder_id`
 GROUP BY `ah`.`id`
