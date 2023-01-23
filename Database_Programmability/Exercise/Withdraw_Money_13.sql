f1b6360b36564f0140c358679acfef78e712def2
Denis Buserski <denis.buserski@gmail.com>
2023-01-23 21:36:23 +0200

    Update Withdraw_Money_13.sql

--- Database_Programmability/Exercise/Withdraw_Money_13.sql
+++ Database_Programmability/Exercise/Withdraw_Money_13.sql
@@ -1,10 +1,12 @@
 CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(20,4))
 BEGIN
 START TRANSACTION;
-	CASE WHEN money_amount < 0 OR 
-              money_amount > (SELECT a.balance FROM accounts as a
-						      WHERE a.id = account_id)
-		THEN ROLLBACK;
+	CASE WHEN 
+	     money_amount < 0 OR 
+             money_amount > (SELECT a.balance 
+			     FROM accounts as a
+			     WHERE a.id = account_id)
+	THEN ROLLBACK;
 	ELSE 
 		UPDATE accounts as a 
 		SET a.balance = a.balance - money_amount
