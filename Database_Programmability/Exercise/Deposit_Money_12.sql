01e6d94cd8a639e33e1d2af8629830e631b703d5
Denis Buserski <denis.buserski@gmail.com>
2023-01-23 21:34:21 +0200

    Update Deposit_Money_12.sql

--- Database_Programmability/Exercise/Deposit_Money_12.sql
+++ Database_Programmability/Exercise/Deposit_Money_12.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DECIMAL(20,4))
 BEGIN
 START TRANSACTION;
 	CASE WHEN money_amount < 0 
-		THEN ROLLBACK;
+	     THEN ROLLBACK;
 	ELSE 
 		UPDATE accounts as a 
 		SET a.balance = a.balance + money_amount
