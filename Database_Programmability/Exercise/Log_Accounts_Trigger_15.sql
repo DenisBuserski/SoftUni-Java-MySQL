6cbedebcfe62a20ab2d95dfed28d056c6c6eef24
Denis Buserski <denis.buserski@gmail.com>
2023-01-23 21:37:45 +0200

    Update Log_Accounts_Trigger_15.sql

--- Database_Programmability/Exercise/Log_Accounts_Trigger_15.sql
+++ Database_Programmability/Exercise/Log_Accounts_Trigger_15.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `logs` (
-    `log_id` INT PRIMARY KEY AUTO_INCREMENT,
-    `account_id` INT NOT NULL,
-    `old_sum` DECIMAL(19 , 4 ) NOT NULL,
-    `new_sum` DECIMAL(19 , 4 ) NOT NULL
+`log_id` INT PRIMARY KEY AUTO_INCREMENT,
+`account_id` INT NOT NULL,
+`old_sum` DECIMAL(19 , 4 ) NOT NULL,
+`new_sum` DECIMAL(19 , 4 ) NOT NULL
 );
-
 
 CREATE TRIGGER trigger_balance_update
 AFTER UPDATE ON `accounts`
