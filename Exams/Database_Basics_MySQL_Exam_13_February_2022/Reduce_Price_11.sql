be74a2452e34eb1208b2e5fd6c931ef08e64a669
Denis Buserski <denis.buserski@gmail.com>
2023-01-24 07:31:34 +0200

    Update and rename Database_Basics_MySQL_Exam_13_February_2022/Reduce_Price_11.sql to Exams/Database_Basics_MySQL_Exam_13_February_2022/Reduce_Price_11.sql

--- Exams/Database_Basics_MySQL_Exam_13_February_2022/Reduce_Price_11.sql
+++ Exams/Database_Basics_MySQL_Exam_13_February_2022/Reduce_Price_11.sql
@@ -5,6 +5,7 @@ UPDATE
 JOIN `reviews` AS `r` ON `p`.`review_id` = `r`.`id`
 JOIN `categories` AS `c` ON `c`.`id` = `p`.`category_id`
 SET `p`.`price` = `p`.`price` - (0.3 * `p`.`price`)
-WHERE `r`.`rating` < 4 AND
-`c`.`name` = category_name;
+WHERE 
+`r`.`rating` < 4 
+AND `c`.`name` = category_name;
 END
